bbb99905b29614c9e3371ad0e67be4fadf34fbe9
Min RK <benjaminrk@gmail.com>
2020-03-06 14:56:44 +0100

    remove outdated rackspace commands

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# nbviewer-deploy
+# nbviewer.org-deploy
 
-Tasks for running nbviewer in docker, with [invoke](http://pyinvoke.org).
+Tasks for running nbviewer.jupyter.org in helm, with [invoke](http://pyinvoke.org).
 
-The tasks have two categories:
-
-1. managing nbviewer servers on rackspace (run from your laptop), and
-2. launching/upgrading nbviewer itself in docker (run on the remote via docker-machine)
+TODO: helm commands have not yet been implemented.
 
 ## Quickstart: upgrading nbviewer
 
@@ -18,7 +15,7 @@ publishing the latest version of nbviewer can be done with one command from this
 
     invoke doitall
 
-This will:
+This will: TODO
 
 1. `git pull`
 2. `invoke upgrade` on each nbviewer instance, via docker-machine
@@ -27,68 +24,13 @@ There will be a confirmation prompt once it gets to the destructive action of de
 
 ## Current deployment
 
-Right now, nbviewer is run on Rackspace servers on the Jupyter account, with the names 'nbviewer-N'.
-
-On each server, there are two nbviewer workers and one memcache instance. One server is additionally running the statuspage daemon to send stats to https://status.jupyter.org,
-started with
-
-    invoke statuspage
-
-Each server is forwarding its logs to `logentries`, as well.
+Right now, nbviewer is run on OVHCloud via helm in the namespace `nbviewer`.
 
 ## Dependencies
 
 Python dependencies:
 
     pip install -r requirements.txt
-
-
-## Managing nbviewer servers
-
-nbviewer servers are hosted on Rackspace and created with `docker-machine`.
-The credentials are stored in this repo in the `machine` directory.
-
-There are a few commands that can be run from your laptop,
-to help with managing nbviewer servers.
-The rest talk to docker directly, and should be run after setting up your docker env for a given node:
-
-    eval $(invoke env nbviewer-3)
-
-To see the names and ips of current nbviewer servers:
-
-    invoke servers
-
-### Adding a node
-
-    invoke add_node
-
-will allocate a new node with `docker-machine`,
-start nbviewer on it, and add it to fastly.
-
-Remember to commit the changes to the `machine` credentials directory when you do this.
-
-### Removing a node
-
-    invoke remove_node nbviewer-1
-
-will teardown a server and remove it from fastly.
-
-Remember to commit the changes to the `machine` credentials directory when you do this.
-
-## Running nbviewer with docker
-
-The commands below talk to a single docker instance, and are to be run with access
-to docker, either directly or via `docker-machine`.
-
-### Booting from scratch
-
-Assuming docker is set up, run
-
-```
-invoke bootstrap -n 2
-```
-
-to start a new deployment of nbviewer with two instances.
 
 
 ### Upgrading
@@ -99,18 +41,7 @@ To upgrade the deployment in-place:
 invoke upgrade
 ```
 
-This will pull new images from DockerHub, take down containers one at a time, and bring new ones up in their places.
-
-TODO: we should actually bring up new nodes on *new* ports and add them to fastly before removing the old ones. This should be doable now that fastly is scripted.
-
-### Restart
-
-To relaunch the current instances without any other changes:
-
-```
-invoke restart
-```
-
+This will deploy the new helm configuration
 
 ## TODO
 
--- tasks.py
+++ tasks.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Deploys nbviewer on docker
+Deploys nbviewer on helm
 
-Two pieces:
-
-First set of commands help setup nbviewer servers on Rackspace:
-
-    servers, new_machine, doitall
-
-Second set are for deploying nbviewer with docker on a single node
-(assumes docker env is already setup):
-
-    build, pull, nbviewer, upgrade, bootstrap, etc.
 """
 
 from __future__ import print_function
@@ -25,45 +15,17 @@ import re
 import socket
 import sys
 import time
+
 join = os.path.join
 
 from invoke import run, task
-from docker import APIClient as Client
-from docker.utils import kwargs_from_env
-from machine import Machine
 import requests
 
-
-NBVIEWER = 'jupyter/nbviewer'
-NBCACHE = 'jupyter/nbcache'
-LOGENTRIES = 'logentries/docker-logentries'
-NODE_FLAVOR = 'general1-2'
-HERE = os.path.dirname(os.path.abspath(__file__))
-MACHINE_DIR = 'machine'
-os.environ['MACHINE_STORAGE_PATH'] = MACHINE_DIR
-
-SERVER_NAME_PAT = re.compile(r'nbviewer\-(\d+)')
 
 creds = {}
 with open('creds') as f:
     exec(f.read(), creds)
 
-#------- Local commands for managing rackspace servers --------
-
-@lru_cache()
-def rackspace_client():
-    from rackspace.connection import Connection
-
-    return Connection(
-        username=creds['OS_USERNAME'],
-        api_key=creds['OS_PASSWORD'],
-        region='DFW',
-    )
-
-@lru_cache()
-def nbviewer_servers():
-    c = rackspace_client()
-    return list(c.compute.servers(name='nbviewer'))
 
 @task
 def trigger_build(ctx):
@@ -72,268 +34,34 @@ def trigger_build(ctx):
     r.raise_for_status()
     print(r.text)
 
-@task
-def new_machine(ctx):
-    """Allocate a new server with docker-machine"""
-    existing_server_ids = [
-        int(SERVER_NAME_PAT.match(s.name).group(1))
-        for s in nbviewer_servers()
-    ]
-    server_id = max(existing_server_ids) + 1
-    name = 'nbviewer-%s' % server_id
-    env = {}
-    env['OS_USERNAME'] = creds['OS_USERNAME']
-    env['OS_API_KEY'] = creds['OS_PASSWORD']
-    env['OS_FLAVOR_ID'] = NODE_FLAVOR
-    
-    rc = rackspace_client()
-    images = [ image for image in rc.compute.images() if 'Ubuntu 18.04' in image.name]
-    image = [ image for image in images if 'PVHVM' in image.name][0]
-    env['OS_IMAGE_ID'] = image.id
-    env['OS_REGION_NAME'] = "DFW"
-    ctx.run('docker-machine create %s --driver=rackspace' % name, env=env, echo=True)
-    return name
-
-@task
-def add_node(ctx):
-    """Add a node to the cluster"""
-    name = new_machine(ctx)
-    ctx.run("""
-        eval $(docker-machine env %s)
-        invoke bootstrap
-    """ % name, echo=True)
-    # add it to fastly
-    fastly(ctx)
-
-@task
-def remove_node(ctx, name):
-    """Remove a docker machine"""
-    ctx.run('docker-machine rm %s' % name)
-    fastly(ctx)
-
-@task
-def env(ctx, name):
-    """`docker-machine env` with our machine path
-    
-    Run `eval $(invoke env nbviewer1)` to setup docker env for a given nbviewer instance
-    
-    Same as MACHINE_STORAGE_PATH=$PWD/machines docker-machine env <name>
-    """
-    ctx.run('docker-machine env %s' % name)
-
-
-@task
-def servers(ctx):
-    """Print the names of current nbviewer servers"""
-    for server in nbviewer_servers():
-        print(server.name, server.access_ipv4)
-
-def docker_machines(ctx):
-    """Get list of docker-machine names"""
-    return ctx.run('docker-machine ls -q', hide='out').stdout.split()
 
 @task
 def doitall(ctx):
     """Run a full upgrade from your laptop.
-    
+
     This does:
-    
+
     1. git pull
     2. upgrade on all machines
     """
     # make sure current repo is up to date
     ctx.run('git pull', echo=True)
-    for machine in docker_machines(ctx):
-        ctx.run("""
-        set -e
-        eval $(docker-machine env %s)
-        invoke upgrade
-        """ % machine, echo=True)
+    upgrade(ctx)
     fastly(ctx)
 
-#------- Docker commands for running nbviewer --------
-
-@lru_cache()
-def docker_client():
-    """Get a docker client instance"""
-    docker = Client(**kwargs_from_env())
-    return docker
-
-@task
-def nbcache(ctx):
-    """Start the nbcache service"""
-    run("docker run -d --label nbcache --name nbcache --restart always %s" % NBCACHE)
-
-@task
-def logentries(ctx):
-    """Start the logentries log-forwarding service"""
-    run("docker run -d --label logentries --name logentries --restart always "
-        "-v /var/run/docker.sock:/var/run/docker.sock "
-        "%s -t %s --no-stats -j -a host=%s" % (
-            LOGENTRIES, creds['LOGENTRIES_TOKEN'],
-            os.environ.get('DOCKER_MACHINE_NAME', socket.gethostname())
-        )
-    )
-
-@task
-def nbviewer(ctx, port=0, image='nbviewer'):
-    """Start one nbviewer instance"""
-    docker = docker_client()
-    containers = docker.containers(filters={'label': 'nbcache'})
-    if not containers:
-        nbcache(ctx)
-    containers = docker.containers(filters={'label': 'nbcache'})
-    nbcache_id = containers[0]['Id']
-    
-    nbviewers = docker.containers(filters={'label': 'nbviewer'})
-    ports_in_use = set()
-    for c in nbviewers:
-        for port_struct in c['Ports']:
-            ports_in_use.add(port_struct.get('PublicPort'))
-    
-    if port == 0:
-        port = 8080
-        while port in ports_in_use:
-            port += 1
-
-    run(' '.join(map(pipes.quote, ['docker', 'run', '-d',
-        '--env-file', 'env_file',
-        '--label', 'nbviewer',
-        '--restart', 'always',
-        '--link', '%s:nbcache' % nbcache_id,
-        '-p', '%i:8080' % port,
-        '--name', 'nbviewer-%i' % port,
-        image,
-        'newrelic-admin', 'run-python',
-        '-m', 'nbviewer',
-        '--logging=info',
-        '--port=8080',
-        '--cache_expiry_min=3600',
-        '--cache_expiry_max=14400',
-        "--content_security_policy=connect-src *", # https://github.com/jupyter/nbviewer/issues/797    
-        '--jupyter_js_widgets_version=2.1', # https://github.com/jupyter/nbviewer/issues/818
-        '--jupyter_widgets_html_manager_version=0.15', # https://github.com/jupyter/nbviewer/issues/818
-    ])), echo=True)
-    return port
-
-@task
-def pull(ctx, images=','.join([NBVIEWER, NBCACHE])):
-    """Pull any updates to images from DockerHub"""
-    for img in images.split(','):
-        if '/' not in img:
-            img = 'jupyter/' + img
-        run('docker pull %s' % img)
-
-@task
-def build(ctx):
-    """Build nbviewer image"""
-    run('docker build -t nbviewer .')
-
-@task
-def bootstrap(ctx, n=2):
-    """Set up a new cluster with nbcache, nbviewer"""
-    pull(ctx)
-    build(ctx)
-    nbcache(ctx)
-    logentries(ctx)
-    ports = []
-    for i in range(n):
-        ports.append(nbviewer(ctx))
-    print("Started %i nbviewer instances on ports %s" % (n, ports))
-
-def wait_up(url):
-    print("Waiting for %s" % url, end='')
-    for i in range(30):
-        try:
-            requests.get(url)
-        except Exception:
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            time.sleep(1)
-        else:
-            print(".ok")
-            return
-    raise RuntimeError("nbviewer never showed up at %s" % url)
 
 @task
 def upgrade(ctx, yes=False):
-    """Update images and redeploy
-    
-    NOTE: This destroys the old instances, so don't forget to download their logs first if you need them.
+    """Update helm deployment
+
     """
-    pull(ctx)
-    build(ctx)
-    docker = docker_client()
-    containers = docker.containers(filters={'label': 'nbviewer'})
-    if not yes:
-        ans = ''
-        q = "Are you sure? This will delete (including logs) %i containers [y/n]" % len(containers)
-        while not ans.lower().startswith(('y','n')):
-            ans = input(q)
-        if ans.lower()[0] == 'n':
-            return
-    ports = []
-    for running in containers:
-        interface = running['Ports'][0]
-        ip = interface.get('IP')
-        if ip == '0.0.0.0':
-            ip = ctx.run('docker-machine ip $(docker-machine active)', hide='out').stdout.strip()
-        port = interface.get('PublicPort')
-        id = running['Id']
-        print("Relaunching %s at %s:%i" % (id[:7], ip, port))
-        docker.stop(id)
-        docker.remove_container(id)
-        nbviewer(ctx, port)
-        url = 'http://%s:%i' % (ip, port)
-        wait_up(url)
-        ports.append(port)
-    print("Upgraded %i instances at %s" % (len(containers), ports))
+    raise NotImplementedError("Not implemented yet for helm")
 
-@task
-def restart(ctx):
-    """Restart the current nbviewer instances"""
-    docker = docker_client()
-    containers = docker.containers(all=True, filters={'label': 'nbviewer'})
-    for c in containers:
-        short = c['Id'][:7]
-        print("restarting %s" % short)
-        docker.restart(c['Id'])
 
-@task
-def cleanup(ctx):
-    """Cleanup stopped containers"""
-    docker = docker_client()
-    containers = docker.containers(filters={'label': 'nbviewer', 'status': 'exited'})
-    if not containers:
-        print("No containers to cleanup")
-    for c in containers:
-        id = c['Id']
-        print("Removing %s" % id[:7])
-        docker.remove_container(id)
-
-@task
-def statuspage(ctx):
-    """Run the statuspage container. Only need one of these total."""
-    docker = docker_client()
-    stream = docker.build('statuspage', tag='nbviewer-statuspage', pull=True)
-    for chunk in stream:
-        for line in chunk.decode('utf8', 'replace').splitlines(True):
-            info = json.loads(line)
-            sys.stdout.write(info.get('stream', ''))
-    containers = docker.containers(all=True, filters={'name': 'nbviewer-statuspage'})
-    [ docker.remove_container(c['Id'], force=True) for c in containers ]
-    run(' '.join(map(pipes.quote, ['docker', 'run',
-        '-d', '-t',
-        '--restart=always',
-        '--env-file=env_file',
-        '--env-file=env_statuspage',
-        '--name=nbviewer-statuspage',
-        'nbviewer-statuspage'
-    ])))
-
-#------- Fastly commands for updating the CDN --------
+# ------- Fastly commands for updating the CDN --------
 
 FASTLY_API = 'https://api.fastly.com'
+
 
 class FastlyService:
     def __init__(self, api_key, service_id):
@@ -348,7 +76,6 @@ class FastlyService:
             latest_version = self.versions()[-1]
             self.version = latest_version['number']
 
-    
     def api_request(self, path, include_version=True, method='GET', **kwargs):
         url = "{api}/service/{service_id}{v}{path}".format(
             api=FASTLY_API,
@@ -363,13 +90,13 @@ class FastlyService:
             print(r.text)
             raise
         return r.json()
-    
+
     def backends(self):
         return self.api_request('/backend')
-    
+
     def versions(self):
         return self.api_request('/version', include_version=False)
-    
+
     def add_backend(self, name, hostname, port, copy_backend=None):
         if copy_backend is None:
             copy_backend = self.backends()[0]
@@ -384,19 +111,14 @@ class FastlyService:
                 'between_bytes_timeout',
                 'first_byte_timeout',
                 'auto_loadbalance',
-                
             ]
         }
-        data.update({
-            'address': hostname,
-            'name': name,
-            'port': port,
-        })
+        data.update({'address': hostname, 'name': name, 'port': port})
         self.api_request('/backend', method='POST', data=data)
-    
+
     def remove_backend(self, name):
         self.api_request('/backend/%s' % name, method='DELETE')
-    
+
     def deploy(self):
         # activate the current version
         self.api_request('/activate', method='PUT')
@@ -410,33 +132,10 @@ def all_instances():
     all_nbviewers = {}
     docker_machine = Machine()
     # add ovh by hand
+    # TODO: get service from kubernetes
     all_nbviewers[('in51bdkev0.lb.c1.gra.k8s.ovh.net', 80)] = 'ovh'
-    for m in docker_machine.ls():
-        name = m['Name']
-        if not name:
-            # weird bug where it gets an extra empty entry
-            continue
-        ip = docker_machine.inspect(name)['Driver']['IPAddress']
-        docker = Client(**docker_machine.config(name))
-        for c in docker.containers(filters={'label': 'nbviewer'}, all=True):
-            container_name = c['Names'][0].lstrip('/')
-            if c['State'] != 'running':
-                print((
-                    "{container} on {machine} is stopped (state={state})!\n"
-                    "You may want to start it manually with:\n"
-                    "    eval $(invoke env {machine})\n"
-                    "    docker start {container}\n"
-                    "or inspect the logs with:\n"
-                    "    eval $(invoke env {machine})\n"
-                    "    docker logs --tail 100 {container}\n"
-                ).format(
-                    container=container_name, machine=m['Name'], state=c['State'],
-                ))
-                continue
-
-            port = c['Ports'][0].get('PublicPort')
-            all_nbviewers[(ip, port)] = '%s-%s' % (name, port)
     return all_nbviewers
+
 
 @task
 def fastly(ctx):
