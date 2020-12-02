b7c9fcf92acb568fbb647c2b952c48ae57543df2
Min RK <benjaminrk@gmail.com>
2020-12-02 10:30:25 +0100

    add short-term deploy.sh
    not ready for ci, but stores long commands, helm diff

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+
+# TODO: move this to CI, don't make assumptions about local repo checkouts
+
+export KUBECONFIG=$PWD/secrets/ovh-kubeconfig.yaml
+nbviewer_chart="../nbviewer/helm-chart/nbviewer"
+echo "Is $PWD/../nbviewer up to date?"
+helm dep up $nbviewer_chart
+
+upgrade="upgrade nbviewer $nbviewer_chart -f config/nbviewer.yaml -f secrets/config/nbviewer.yaml"
+helm diff $upgrade | grep -v '^[[:space:]]'
+
+echo "Deploy these changes? (y|[N]) "
+read confirm
+
+if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
+  echo "Upgrading..."
+  helm $upgrade
+else
+  echo "Cancelled"
+  exit 1
+fi
+
+# watch deployment rollout
+kubectl rollout status -w deployment/nbviewer
